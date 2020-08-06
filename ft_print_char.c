d24958bfc9780300468778eb3038d5f6b75f7ddc
rpunet <r.punetcorvillo@gmail.com>
2020-08-06 02:32:05 +0200

    printf3

---
+++ ft_print_char.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_char.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpunet <rpunet@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/06 01:15:48 by rpunet            #+#    #+#             */
+/*   Updated: 2020/08/06 01:23:58 by rpunet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	char_convert(t_struct *s, va_list ap)
+{
+	unsigned char	a;
+
+	a = (unsigned char)va_arg(ap, int);
+	if (s->neg == 0)
+	{
+		while (s->width-- > 1)
+			s->ret += write(1, " ", 1);
+	}
+	s->ret += write(1, &a, 1);
+	if (s->neg == 1)
+	{
+		while (s->width-- > 1)
+			s->ret += write(1, " ", 1);
+	}
+}
